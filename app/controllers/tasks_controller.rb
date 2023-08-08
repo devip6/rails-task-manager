40ea7aa3e644a6cce3611decbc55971b6b9454ac
Devi Putri <devi.putri@live.com>
2023-08-08 16:55:33 +0900

    commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+
+  def index
+    @task = Task.all
+
+  end
+
+  def detail
+    @task = Task.find(params[:id])
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
